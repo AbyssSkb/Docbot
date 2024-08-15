37ea7553739f8bcf5417d22b1cc5a64511f52652
Abyss Skb <abyssskb@outlook.com>
2024-08-15 16:36:46 +0800

    new

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # AI 文档机器人
 ## 功能
-能够读取文件夹下的 txt 文档来回答问题
+能够读取文件夹下的文档来回答问题
 ## 原理
 1. 读取文档并切成块
 2. 采用多路召回的方法，使用 `GTE + BGE + BM25`，提高召回率
-3. 将召回的文本同问题合并在一起作为大模型的输入，最终输出答案
+3. 使用 `Reranker` 将多路召回的文本进行重排序，选出前几个文本
+4. 将文本同问题合并在一起作为大模型的输入，最终输出答案
 ## 使用
 1. 克隆仓库
 ```bash
 git clone https://github.com/AbyssSkb/Docbot
 ```
-2. 安装所需的库
+1. 安装所需的库
 ```bash
 cd Docbot
 pip install -r requirements.txt
 ```
-3. 将 txt 文档放入 `doc` 文件夹下
-4. 运行 `embedding.py`，将文档切成块，生成 GTE 和 BGE 所需的 embedding，最终保存下来。
-5. 修改 `main.py` 中的 `api_key`，如有需要，可以修改 `base_url` 以及 `llm_model`
-6. 运行 `main.py`
-7. 在命令行中输入你想问的问题
+1. 将文档放入 `doc` 文件夹下（如果 `doc` 文件夹不存在，请先创建）
+2. 运行 `create_index.py`，将文档切成块，生成 GTE 和 BGE 所需的 embedding，最终保存成索引
+3. 修改 `main.py` 中的 `api_key`，如有需要，可以修改 `base_url` 以及 `llm_model`
+4. 运行 `main.py`
+5. 在命令行中输入你想问的问题
   
--- create_index.py
+++ create_index.py
@@ -2,14 +2,13 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from model import EmbeddingModel
 import json
+import os
 
 embed1 = EmbeddingModel('thenlper/gte-large-zh')
 embed2 = EmbeddingModel('BAAI/bge-large-zh-v1.5')
-models = [embed1, embed2]
 
 if __name__ == '__main__':
-    document_dir = 'doc'
-    loader = DirectoryLoader(document_dir)
+    loader = DirectoryLoader('doc')
     document = loader.load()
 
     chunk_size = 512
@@ -17,8 +16,10 @@ if __name__ == '__main__':
     chunks = text_splitter.split_documents(document)
     texts = [chunk.page_content for chunk in chunks]
 
+    if not os.path.exists('index'):
+        os.mkdir('index')
     embed1.save_index(texts, 'index/embed1.index')
     embed2.save_index(texts, 'index/embed2.index')
 
-    with open('texts.json', 'w', encoding='utf-8') as f:
+    with open('index/texts.json', 'w', encoding='utf-8') as f:
         json.dump(texts, f)
